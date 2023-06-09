4ad1748909dc0692681eb6bee09d411f87998f3f
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-06-09 13:48:42 +0600

    Update start_cmd.py

--- cmd/start_cmd.py
+++ cmd/start_cmd.py
@@ -1,3 +1,2 @@
 def start(update, context):
-    message = "Hello! Welcome to the bot. How can I assist you today?"
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome! This is the start command.")
